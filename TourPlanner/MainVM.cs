fb982d25a18e0fbf9e0f5c380442e34bbca78d30
alobre <51901804+alobre@users.noreply.github.com>
2023-03-29 17:23:42 +0200

    starting basic MVVM Setup
    using: https://www.youtube.com/watch?v=CABv5xIDC08 as guide

---
+++ TourPlanner/MainVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace TourPlanner
+{
+    internal class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this.PropertyChanged, new PropertyChangedEventArgs(propName));
+        }
+
+        private Route route;
+
+        public Route Route
+        {
+            get { return route; }
+            set { route = value; OnPropertyChanged(nameof(Route)); }
+        }
+
+        public ICommand CMDAdd { get; set; }
+
+    }
+}
---
+++ TourPlanner/Route.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace TourPlanner
+{
+    public class Route : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this.PropertyChanged, new PropertyChangedEventArgs(propName));
+        }
+
+        private string _Start;
+		public string Start
+		{
+			get { return _Start; }
+			set { _Start = value; OnPropertyChanged(nameof(Start)); }
+		}
+        private string _Destination;
+        public string Destination
+        {
+            get { return _Destination; }
+            set { _Destination = value; OnPropertyChanged(nameof(Destination)); }
+        }
+
+    }
+}
