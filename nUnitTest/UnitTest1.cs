6f0c16460c732a1fc735cf4e76f9108200da1758
wsowy <51901804+alobre@users.noreply.github.com>
2023-06-20 00:09:19 +0200

    Added sample Test

--- nUnitTest/UnitTest1.cs
+++ nUnitTest/UnitTest1.cs
@@ -9,9 +9,10 @@ namespace nUnitTest
         }
 
         [Test]
-        public void Test1()
+        public void AddToursCheckIfContent()
         {
-            Assert.Pass();
+            Tour tour = new Tour(name: "Test1", tour_desc: "description", from:"testFrom", to: "testTo", distance: 2, image_link: "www.link.net", transport_type: "testo", time:10, route_information: new BitmapImage());
+            Assert.IsTrue(tour.Name == "Test1");
         }
     }
 }
--- nUnitTest/Usings.cs
+++ nUnitTest/Usings.cs
@@ -1,2 +1,3 @@
 global using NUnit.Framework;
 global using TourPlanner.BL.Models;
+global using System.Windows.Media.Imaging;
